6ed6384a1e53d4cba44a47d43edf57d1d3956410
Bogdan Fasie, INI-DOS-DE-TEO (EXT) <bogdan.fasie@swisscom.com>
2022-11-27 12:51:47 +0200

    add logs

--- src/main/java/com/order/product/api/OrderProductController.java
+++ src/main/java/com/order/product/api/OrderProductController.java
@@ -2,10 +2,12 @@ package com.order.product.api;
 
 import com.order.product.api.dto.ProductDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class OrderProductController implements OrderProductManager {
@@ -13,12 +15,16 @@ public class OrderProductController implements OrderProductManager {
 
 	@Override
 	public List<ProductDto> getProducts() {
-		return service.getProducts();
+		log.info("getProducts()");
+		List<ProductDto> productDtoList = service.getProducts();
+		log.debug("getProducts() finished");
+		return productDtoList;
 	}
 
 	@Override
 	public void addProduct(ProductDto productDto) {
+		log.info("addProduct({})", productDto);
 		service.addProduct(productDto);
+		log.debug("addProduct() finished");
 	}
-
 }
