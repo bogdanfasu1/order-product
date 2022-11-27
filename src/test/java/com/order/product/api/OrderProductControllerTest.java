8a68dfb0337e8273e0d8ba28d008cbd96b99ad09
Bogdan Fasie, INI-DOS-DE-TEO (EXT) <bogdan.fasie@swisscom.com>
2022-11-27 15:26:34 +0200

    add unittests fro controller

---
+++ src/test/java/com/order/product/api/OrderProductControllerTest.java
@@ -0,0 +1,120 @@
+package com.order.product.api;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.order.product.api.dto.ProductDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class OrderProductControllerTest {
+	private static final String PATH = "/orders";
+	private static final String PRODUCT_ID = "/1";
+	private static final String NAME = "name";
+	private static final String DESCRIPTON = "desc";
+	private static final Double PRICE = 2.5;
+	private ObjectMapper objectMapper;
+
+	private MockMvc mockMvc;
+	@InjectMocks
+	private OrderProductController orderProductController;
+	@Mock
+	private ProductService service;
+
+	@BeforeEach
+	void setUp() {
+		mockMvc = MockMvcBuilders.standaloneSetup(orderProductController).build();
+		objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+	}
+
+	@Test
+	void getProductsTest() throws Exception {
+		mockMvc.perform(get(PATH)
+						.characterEncoding("UTF-8"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andReturn();
+	}
+
+	@Test
+	void getProductByIdTest() throws Exception {
+		mockMvc.perform(get(PATH + PRODUCT_ID)
+						.characterEncoding("UTF-8"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andReturn();
+	}
+
+	@Test
+	void deleteProductByIdTest() throws Exception {
+		mockMvc.perform(delete(PATH + PRODUCT_ID)
+						.characterEncoding("UTF-8"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andReturn();
+	}
+
+	@Test
+	public void addProductTest() throws Exception {
+		ProductDto productDto = getProductDto();
+		mockMvc.perform(post(PATH)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(productDto)))
+				.andExpect(status().is(204));
+	}
+
+	@Test
+	public void addProduct_shouldReturnBadRequest() throws Exception {
+		ProductDto productDto = getWrongProductDto();
+		mockMvc.perform(post(PATH)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(productDto)))
+				.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	public void modifyProductTest() throws Exception {
+		ProductDto productDto = getProductDto();
+		mockMvc.perform(put(PATH + PRODUCT_ID)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(productDto)))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void modifyProduct_shouldReturnBadRequest() throws Exception {
+		ProductDto productDto = getWrongProductDto();
+		mockMvc.perform(put(PATH + PRODUCT_ID)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(productDto)))
+				.andExpect(status().isBadRequest());
+	}
+
+	private static ProductDto getProductDto() {
+		return ProductDto.builder()
+				.price(PRICE)
+				.description(DESCRIPTON)
+				.name(NAME)
+				.build();
+	}
+
+	private static ProductDto getWrongProductDto() {
+		return ProductDto.builder()
+				.price(PRICE)
+				.name(NAME)
+				.build();
+	}
+}
